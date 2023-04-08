50c3e44c8e2d613b6918bda7827ae761d3ac49dd
Tony Theodoropoulos <theodot@outlook.com>
2023-04-08 14:25:03 -0400

    Add project files.

---
+++ StaticClassExample/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassExample
+{
+    internal class Calculator
+    {
+        static private double gravity = 9.8;
+
+        static public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static public int Subtract(int x, int y)
+        {
+            int difference;
+
+            difference = x - y;
+            return difference;
+        }
+
+        static public double Velocity(double seconds)
+        {
+            double speed = gravity * seconds;
+            return speed;
+        }
+
+        static public void SetGravity(double newGravity)
+        {
+            gravity = newGravity;
+        }
+    }
+}
---
+++ StaticClassExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer;
+            double time;
+            double speed;
+            double gravity;
+
+            answer = Calculator.Add(3, 4);
+            Console.WriteLine(answer);
+
+            answer = Calculator.Subtract(3, 4);
+            Console.WriteLine(answer);
+            
+            do
+            {
+                Console.WriteLine("Please enter a gravity");
+                gravity = Convert.ToDouble(Console.ReadLine());
+                Calculator.SetGravity(gravity);
+
+                Console.WriteLine("Please enter a time");
+                time = Convert.ToDouble(Console.ReadLine());
+
+                speed = Calculator.Velocity(time);
+                Console.WriteLine(speed + "m/s");
+
+            } while (time != 0);
+
+            Console.ReadKey();
+        }
+    }
+}
